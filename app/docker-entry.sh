a047a4b7d8a618826ef8a50bc9de14b233384d14
jaekwon-park <insainty21@gmail.com>
2016-03-24 20:48:39 +0900

    change database configuration

--- app/docker-entry.sh
+++ app/docker-entry.sh
@@ -4,17 +4,20 @@ echo -ne "Configure database connection..."
 if [ ! -f $config ]
 then
     cp /config.template.json $config
-    sed -Ei "s/DB_PORT/$DB_PORT_5432_TCP_PORT/" $config
+    sed -Ei "s/MYSQL_USER/$MYSQL_USER/" $config
+    sed -Ei "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/" $config
+    sed -Ei "s/MYSQL_DATABASE/$MYSQL_DATABASE/" $config
+    sed -Ei "s/MYSQL_HOST/$MYSQL_HOST/" $config
     echo OK
 else
     echo SKIP
 fi
 
-echo "Wait until database is ready..."
-until nc -z db $DB_PORT_5432_TCP_PORT
-do
-    sleep 1
-done
+#echo "Wait until database is ready..."
+#until nc -z db $DB_PORT_5432_TCP_PORT
+#do
+#    sleep 1
+#done
 
 # Wait to avoid "panic: Failed to open sql connection pq: the database system is starting up"
 sleep 1
