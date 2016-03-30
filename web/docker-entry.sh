bdc0884ac5784d28c6cd4ecc4e8d8d7c9b0d03de
jaekwon-park <insainty21@gmail.com>
2016-03-31 02:11:11 +0900

    Update docker-entry.sh

--- web/docker-entry.sh
+++ web/docker-entry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo Starting Nginx
-sed -Ei "s/PLATFORM_ADDR/$PLATFORM_PORT_80_TCP_ADDR/" /etc/nginx/sites-available/mattermost
-sed -Ei "s/PLATFORM_PORT/$PLATFORM_PORT_80_TCP_PORT/" /etc/nginx/sites-available/mattermost
+sed -Ei "s/PLATFORM_ADDR/$PLATFORM_PORT_80_TCP_ADDR/" /etc/nginx/sites-available/rocketchat
+sed -Ei "s/PLATFORM_PORT/$PLATFORM_PORT_80_TCP_PORT/" /etc/nginx/sites-available/rocketchat
 nginx -g 'daemon off;'
