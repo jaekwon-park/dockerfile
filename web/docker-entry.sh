1867bb3030e126da80962c5b51ecee867dee0eb6
jaekwon-park <insainty21@gmail.com>
2016-03-31 14:39:20 +0900

    Update docker-entry.sh

--- web/docker-entry.sh
+++ web/docker-entry.sh
@@ -2,4 +2,6 @@
 echo Starting Nginx
 sed -Ei "s/PLATFORM_ADDR/$SERVICE_HOST_ENV_NAME/" /etc/nginx/sites-available/rocketchat
 sed -Ei "s/PLATFORM_PORT/$SERVICE_PORT_ENV_NAME/" /etc/nginx/sites-available/rocketchat
+sed -Ei "s/REDIRECT_HOST/$REDIRECT_HOST_ENV_NAME/" /etc/nginx/sites-available/rocketchat
+
 nginx -g 'daemon off;'
